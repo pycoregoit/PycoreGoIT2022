ab90891ebe914dc6f8b22e667006ad67dfb1efbd
pycoregoit <pycoregoit@gmail.com>
2022-10-18 13:57:34 +0300

    first lesson

---
+++ lesson1.py
@@ -0,0 +1,89 @@
+""" Our first lesson """
+
+# Int
+number = 2
+print(number)
+
+print(type(number))
+
+# Float
+number_with_coma = 2.5
+print(number_with_coma)
+print(type(number_with_coma))
+
+# String
+name = "Alisa"
+print(name)
+print(type(name))
+
+# List
+name_of_list = ["Alisa", "Bob"]
+print(name_of_list)
+print(type(name_of_list))
+
+# Tuple
+name_of_tuples = ("Alisa", "Bob")
+print(name_of_tuples)
+print(type(name_of_tuples))
+
+# Dict
+dictionary = {"name": "Alisa", "surname": "Nikols"}
+print(dictionary)
+print(type(dictionary))
+
+# Set
+unique = {1, 2, 3, 3}
+print(unique)
+print(type(unique))
+
+# Bool
+boolean_true = True
+boolean_false = False
+print(boolean_true)
+print(type(boolean_true))
+print(boolean_false)
+print(type(boolean_false))
+
+""" Our first calculation """
+my_salarry = 500
+tax = 0.2
+what_is_taxe = my_salarry * tax
+print("I should pay tax: ", what_is_taxe)
+
+
+
+number = 2
+number_string = str(number)
+print(number_string)
+
+print(type(number_string))
+
+
+str_number = "2"
+str_to_number = int(str_number)
+print(str_to_number)
+
+print(type(str_to_number))
+
+
+str_number = "2"
+new_number = "1"
+sum_numbers = str_number + new_number
+
+print(sum_numbers)
+
+print(type(sum_numbers))
+
+five = 5
+two_half = 2.5
+
+summary = five // two_half
+
+print("Summary ", summary)
+print(type(summary))
+
+
+
+
+
+
